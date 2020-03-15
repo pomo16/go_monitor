daf12f6e34d92ac7de5f24f4437775a2b5e84bfb
pomo <m13143378602@163.com>
2020-03-15 15:49:55 +0800

    fix some config

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ cp -r config output/
 export GO111MODULE=on
 go build -a -o output/${RUN_NAME}
 
-# gojieba 不支持交叉编译
+# 取消交叉编译
 # CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o output/${RUN_NAME}
